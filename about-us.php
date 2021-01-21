a3438b4733789104e87061b1d3c0afd10bdf08fc
salt <ammarhazrinn@gmail.com>
2021-01-21 20:21:52 +0800

    changed filetype

---
+++ about-us.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- SEO Meta Tags -->
+    <meta name="description" content="MyWAVe provides a comprehensive all-in-one Digital Vehicle Lifecycle Management platform">
+    <meta name="author" content="iWorkshop Sdn Bhd">
+
+    <!-- OG Meta Tags to improve the way the post looks when you share the page on LinkedIn, Facebook, Google+ -->
+	<meta property="og:site_name" content="" /> <!-- website name -->
+	<meta property="og:site" content="" /> <!-- website link -->
+	<meta property="og:title" content=""/> <!-- title shown in the actual shared post -->
+	<meta property="og:description" content="" /> <!-- description shown in the actual shared post -->
+	<meta property="og:image" content="" /> <!-- image link, make sure it's jpg -->
+	<meta property="og:url" content="" /> <!-- where do you want your post to link to -->
+	<meta property="og:type" content="article" />
+
+    <!-- Website Title -->
+    <title>About Us - MyWAVe</title>
+    
+    <!-- Styles -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700&display=swap&subset=latin-ext" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/fontawesome-all.css" rel="stylesheet">
+    <link href="css/swiper.css" rel="stylesheet">
+	<link href="css/magnific-popup.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+	
+	<!-- Favicon  -->
+    <link rel="icon" href="">
+</head>
+<body data-spy="scroll" data-target=".fixed-top">
+    
+    <!-- Preloader -->
+	<div class="spinner-wrapper">
+        <div class="spinner">
+            <div class="bounce1"></div>
+            <div class="bounce2"></div>
+            <div class="bounce3"></div>
+        </div>
+    </div>
+    <!-- end of preloader -->
+    
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+        <div class="container">
+
+            <!-- Text Logo - Use this if you don't have a graphic logo -->
+            <!-- <a class="navbar-brand logo-text page-scroll" href="index.html">Tivo</a> -->
+
+            <!-- Image Logo -->
+            <a class="navbar-brand logo-image" href="index.html"><img src="images/logo.png" alt="alternative"></a> 
+            
+            <!-- Mobile Menu Toggle Button -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-awesome fas fa-bars"></span>
+                <span class="navbar-toggler-awesome fas fa-times"></span>
+            </button>
+            <!-- end of mobile menu toggle button -->
+
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="index.html">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="productServices.html">PRODUCTS & SERVICES</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle page-scroll" href="about-us.html" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">ABOUT</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="about-us.html"><span class="item-text">ABOUT US</span></a>
+                            <div class="dropdown-items-divide-hr"></div>
+                            <a class="dropdown-item" href="privacy-policy.html"><span class="item-text">PRIVACY POLICY</span></a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div> 
+    </nav> 
+    
+
+
+    <!-- Header -->
+    <header id="header" class="ex-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>About Us</h1>
+                </div> 
+            </div> 
+        </div> 
+    </header> 
+    
+
+    
+    <!-- Privacy Content -->
+    <div class="ex-basic-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <!-- <div class="image-container">
+                        <div class="video-wrapper">
+                            <a class="popup-youtube" href="https://scontent.fkul13-1.fna.fbcdn.net/v/t66.36240-6/10000000_792316224685018_8945397222955125711_n.mp4?_nc_cat=109&ccb=2&_nc_sid=985c63&efg=eyJ2ZW5jb2RlX3RhZyI6Im9lcF9oZCJ9&_nc_ohc=R_uz_Do5Hy8AX8pevAg&_nc_ht=scontent.fkul13-1.fna&oh=07dbbac5c23baa50cd0cd2d36c6ae3d9&oe=602DEE85" data-effect="fadeIn"></a>
+                                <img class="img-fluid" src="images/i-workshopbg.png" alt="alternative">
+                                <span class="video-play-button">
+                                    <span></span>
+                                </span>
+                            </a>
+                        </div> 
+                    </div>  -->
+                    <div class="text-container">
+                        <div class="section-title text-center pb-25">
+                            <h1>Our Story</h1>
+                        </div>
+                        <br>
+                        <p>iWorkshop Sdn Bhd 201401024952 (1101042-U), a company registered in Malaysia, is an automotive based solution technology and innovation firm. We define success as achieving exceptional results that have a lasting impact on businesses and industries.</p>
+                        <p>iWorkshop’s strength is in its people and our team of Automotive Industry Professionals providing comprehensive expertise in all phases of Automotive Business & Corporate Strategy, Business Restructuring & Blueprint, Technology Development and Project Management. With a diverse range of practical experience, technical training and cultural backgrounds our team is guaranteed to provide maximum quality through the multiple phases of solution and system development.</p>
+                    </div> <!-- end of text-container-->
+                    
+
+                    <div class="text-container dark">
+                        <p class="testimonial-text">We solve the challenges faced by our clients with seamless and end to end services and vigorously endorse business transformation.</p>
+                    </div> <!-- end of text container -->
+
+                    <div class="text-container last">
+                        <h3>Our tagline “Driving Digitalisation” focuses on critical aspect of bridging the digital gaps between people and technology, while ensuring rich user experience, workability, suitability and simplicity.</h3>
+                    </div> <!-- end of text-container -->
+                </div> <!-- end of col-->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of ex-basic-2 -->
+    <!-- end of privacy content -->
+
+    <div class="cards-1">
+        <div class="container">
+            <div class="row">
+                <div class="space"> 
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-image">
+                                <img class="img-fluid" src="images/binoculars.png" alt="alternative">
+                            </div>
+                            <div class="card-body">
+                                <h4 class="card-title">Vision</h4>
+                                <p>To be the preferred After Sales Vehicle Management system by delivering the upmost quality and cohesiveness to all stakeholders</p>
+                            </div>
+                        </div>
+                        
+                        <div class="card">
+                            <div class="card-image">
+                                <img class="img-fluid" src="images/target.png" alt="alternative">
+                            </div>
+                            <div class="card-body">
+                                <h4 class="card-title">Mission</h4>
+                                <p>To provide a comprehensive all-in-one Digital Vehicle Lifecycle Management platform  
+                                </p>
+                            </div>
+                        </div>
+                        
+                        <div class="card">
+                            <div class="card-image">
+                                <img class="img-fluid" src="images/objective.png" alt="alternative">
+                            </div>
+                            <div class="card-body">
+                                <h4 class="card-title">Objective</h4>
+                                <p>Leading the Automotive Industry towards a streamlined path in implementing the relevant pillars of Industry Revolution 4.0 
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </div>
+
+    <div class="row" id="rare-contact">
+        <div class="col-lg-12">
+            <div class="contact-map mt-60">
+                <div class="gmap_canvas">                            
+                    <iframe id="gmap_canvas" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.9093840264222!2d101.59214641475732!3d3.1186693977290925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc4fd7d9d19e3b%3A0x2ad87816febafecd!2sNovutal%20Consulting%20Sdn%20Bhd!5e0!3m2!1sen!2smy!4v1611208694867!5m2!1sen!2smy" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                </div>
+            </div> 
+        </div>
+    </div>
+
+    <section id="contact" class="contact-area pt-125 pb-130 gray-bg">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="section-title text-center pb-25">
+                        <h2 class="title">Get In Touch</h2>
+                        <p>For more information, send us an email and we will get back to you as soon as possible.</p>
+                    </div> 
+                </div>
+            </div> 
+        
+            </div> 
+            <div class="row justify-content-center" id="rare-contact">
+                <div class="col-lg-6">
+                    <div class="contact-form pt-30">
+                        <form id="contactForm" data-toggle="validator" data-focus="false">
+                            <div class="single-form">
+                                <input type="text" name="name" id="cname" placeholder="Name" required>
+                                <div class="help-block with-errors"></div>
+                            </div> 
+                            <div class="single-form">
+                                <input type="email" name="email" id="cemail" placeholder="Email" required>
+                                <div class="help-block with-errors"></div>
+                            </div> 
+                            <div class="single-form">
+                                <textarea name="message" id="cmessage" placeholder="Message" required></textarea>
+                                <div class="help-block with-errors"></div>
+                            </div> 
+                            <div class="single-form">
+                                <button class="main-btn" type="submit">Submit Message</button>
+                            </div> 
+                            <div class="form-message">
+                                <div id="cmsgSubmit" class="h3 text-center hidden"></div>
+                            </div>
+                        </form>
+                    </div> 
+                </div>
+            </div> 
+        </div> 
+    </section>
+
+    
+    <!-- Newsletter -->
+    <div class="form" id="rare-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="icon-container">
+                        <span class="fa-stack">
+                            <a href="#your-link">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-facebook-f fa-stack-1x"></i>
+                            </a>
+                        </span>
+                        <span class="fa-stack">
+                            <a href="#your-link">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-twitter fa-stack-1x"></i>
+                            </a>
+                        </span>
+                        <span class="fa-stack">
+                            <a href="#your-link">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-instagram fa-stack-1x"></i>
+                            </a>
+                        </span>
+                        <span class="fa-stack">
+                            <a href="#your-link">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-linkedin-in fa-stack-1x"></i>
+                            </a>
+                        </span>
+                    </div> <!-- end of col -->
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of form -->
+    <!-- end of newsletter -->
+
+
+    <!-- Footer -->
+    <svg class="footer-frame" id="rare-footer" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" viewBox="0 0 1920 79"><defs><style>.cls-2{fill:#7cc7e2;}</style></defs><title>footer-frame</title><path class="cls-2" d="M0,72.427C143,12.138,255.5,4.577,328.644,7.943c147.721,6.8,183.881,60.242,320.83,53.737,143-6.793,167.826-68.128,293-60.9,109.095,6.3,115.68,54.364,225.251,57.319,113.58,3.064,138.8-47.711,251.189-41.8,104.012,5.474,109.713,50.4,197.369,46.572,89.549-3.91,124.375-52.563,227.622-50.155A338.646,338.646,0,0,1,1920,23.467V79.75H0V72.427Z" transform="translate(0 -0.188)"/></svg>
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="footer-col first">
+                        <h4>About MyWAVe</h4>
+                        <p class="p-small">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae adipisci modi perferendis similique nulla. Dolores cupiditate voluptas quo esse possimus totam ipsa quod non accusantium, obcaecati reiciendis ratione doloribus velit!</p>
+                    </div>
+                </div> <!-- end of col -->
+                <div class="col-md-4">
+                    <div class="footer-col middle">
+                        <h4>Important Links</h4>
+                        <ul class="list-unstyled li-space-lg p-small">
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">Our business partners <a class="white" href="#your-link">MARII</a></div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">Read our <a class="white" href="terms-conditions.html">Terms & Conditions</a>, <a class="white" href="privacy-policy.html">Privacy Policy</a></div>
+                            </li>
+                        </ul>
+                    </div>
+                </div> <!-- end of col -->
+                <div class="col-md-4">
+                    <div class="footer-col last">
+                        <h4>Contact</h4>
+                        <ul class="list-unstyled li-space-lg p-small">
+                            <li class="media">
+                                <i class="fas fa-map-marker-alt"></i>
+                                <div class="media-body">C1-13-7, Block C1, Jalan PJU 1/41, Dataran Prima, 47301 Petaling Jaya, Selangor</div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-envelope"></i>
+                                <div class="media-body"><a class="white" href="mailto:info@mywave.my">info@mywave.my</a> <i class="fas fa-phone"></i><a class="white"> 03-7499 7233</a></div>
+                            </li>
+                        </ul>
+                    </div> 
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of footer -->  
+    <!-- end of footer -->
+
+
+    <!-- Copyright -->
+    <div class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="p-small">Copyright © 2020 iWorkshop Sdn. Bhd.</p>
+                </div> <!-- end of col -->
+            </div> <!-- enf of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of copyright --> 
+    <!-- end of copyright -->
+    
+    	
+    <!-- Scripts -->
+    <script>
+
+    </script>
+    <script src="js/jquery.min.js"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
+    <script src="js/popper.min.js"></script> <!-- Popper tooltip library for Bootstrap -->
+    <script src="js/bootstrap.min.js"></script> <!-- Bootstrap framework -->
+    <script src="js/jquery.easing.min.js"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
+    <script src="js/swiper.min.js"></script> <!-- Swiper for image and text sliders -->
+    <script src="js/jquery.magnific-popup.js"></script> <!-- Magnific Popup for lightboxes -->
+    <script src="js/validator.min.js"></script> <!-- Validator.js - Bootstrap plugin that validates forms -->
+    <script src="js/scripts.js"></script> <!-- Custom scripts -->
+</body>
+</html>

026ca51fb9c87c73e98dfb574818de5cd62b27a6
salt <ammarhazrinn@gmail.com>
2021-01-28 18:57:30 +0800

    hopefully smtp works bruh

--- js/scripts.js
+++ js/scripts.js
@@ -393,60 +393,60 @@
 		$(this).blur();
     });
     
-    $("#contactForm").validator().on("submit", function(event) {
-    	if (event.isDefaultPrevented()) {
-            // handle the invalid form...
-            cformError();
-            csubmitMSG(false, "Please fill all fields!");
-        } else {
-            // everything looks good!
-            event.preventDefault();
-            csubmitForm();
-        }
-    });
+    // $("#contactForm").validator().on("submit", function(event) {
+    // 	if (event.isDefaultPrevented()) {
+    //         // handle the invalid form...
+    //         cformError();
+    //         csubmitMSG(false, "Please fill all fields!");
+    //     } else {
+    //         // everything looks good!
+    //         event.preventDefault();
+    //         csubmitForm();
+    //     }
+    // });
 
-    // contact form
-    function csubmitForm() {
-        // initiate variables with form content
-		var name = $("#cname").val();
-		var email = $("#cemail").val();
-        var message = $("#cmessage").val();
+    // // contact form
+    // function csubmitForm() {
+    //     // initiate variables with form content
+	// 	var name = $("#cname").val();
+	// 	var email = $("#cemail").val();
+    //     var message = $("#cmessage").val();
         
-        $.ajax({
-            type: "POST",
-            url: "php/contactForm.php",
-            data: "name=" + name + "&email=" + email + "&message=" + message, 
-            success: function(text) {
-                if (text == "success") {
-                    cformSuccess();
-                } else {
-                    cformError();
-                    csubmitMSG(false, text);
-                }
-            }
-        });
-	}
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "php/contactForm.php",
+    //         data: "name=" + name + "&email=" + email + "&message=" + message, 
+    //         success: function(text) {
+    //             if (text == "success") {
+    //                 cformSuccess();
+    //             } else {
+    //                 cformError();
+    //                 csubmitMSG(false, text);
+    //             }
+    //         }
+    //     });
+	// }
 
-    function cformSuccess() {
-        $("#contactForm")[0].reset();
-        csubmitMSG(true, "Message Submitted!");
-        $("input").removeClass('notEmpty'); // resets the field label after submission
-        $("textarea").removeClass('notEmpty'); // resets the field label after submission
-    }
+    // function cformSuccess() {
+    //     $("#contactForm")[0].reset();
+    //     csubmitMSG(true, "Message Submitted!");
+    //     $("input").removeClass('notEmpty'); // resets the field label after submission
+    //     $("textarea").removeClass('notEmpty'); // resets the field label after submission
+    // }
 
-    function cformError() {
-        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-            $(this).removeClass();
-        });
-	}
+    // function cformError() {
+    //     $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+    //         $(this).removeClass();
+    //     });
+	// }
 
-    function csubmitMSG(valid, msg) {
-        if (valid) {
-            var msgClasses = "h3 text-center tada animated";
-        } else {
-            var msgClasses = "h3 text-center";
-        }
-        $("#cmsgSubmit").removeClass().addClass(msgClasses).text(msg);
-    }
+    // function csubmitMSG(valid, msg) {
+    //     if (valid) {
+    //         var msgClasses = "h3 text-center tada animated";
+    //     } else {
+    //         var msgClasses = "h3 text-center";
+    //     }
+    //     $("#cmsgSubmit").removeClass().addClass(msgClasses).text(msg);
+    // }
 
 })(jQuery);
---
+++ php/contactForm.php
@@ -0,0 +1,94 @@
+<?php
+use PHPMAiler\PHPMailer\PHPMailer;
+
+if(isset($_POST['name']) && isset($_POST['email'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = "This is a subject";
+    $body = $_POST['body'];
+    
+    require_once "PHPMailer/PHPMailer.php";
+    require_once "PHPMailer/SMTP.php";
+    require_once "PHPMailer/Exception.php";
+
+    $mail = new PHPMailer();
+
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = "biz.merfy@gmail.com";
+    $mail->Password = 'sjjakwgihrskllem';
+    $mail->Port = 465;
+    $mail->SMTPSecure = "ssl";
+
+    $mail->isHTML(true);
+    $mail->setFrom($email, $name);
+    $mail->addAddress("biz.merfy@gmail.com");
+    $mail->Subject = ("$email ($subject)");
+    $mail->Body = $body;
+
+    if($mail->send()){
+        $status = "success";
+        $response = "Email is sent!";
+    }
+    else {
+        $status = "failed";
+        $response = "Something went wrong: <br>" . $mail->ErrorInfo;
+    }
+    exit(json_encode(array("status" => $status, "response" => $response)));
+}
+
+
+
+
+
+// $errorMSG = "";
+// $captcha_error = "";
+
+// if (empty($_POST["name"])) {
+//     $errorMSG = "Name is required ";
+// } else {
+//     $name = $_POST["name"];
+// }
+
+// if (empty($_POST["email"])) {
+//     $errorMSG = "Email is required ";
+// } else {
+//     $email = $_POST["email"];
+// }
+
+// if (empty($_POST["message"])) {
+//     $errorMSG = "Message is required ";
+// } else {
+//     $message = $_POST["message"];
+// }
+
+
+// $EmailTo = "ammarhazrinn@gmail.com";
+// $Subject = "MyWAVe inquiry from";
+
+// // prepare email body text
+// $Body = "Hi Shotbook,\n\nI would like to be included in your awesome ecosystem, my details as follow:\n\n";
+// $Body .= "Name: ";
+// $Body .= $name;
+// $Body .= "\n\n";
+// $Body .= "Email: ";
+// $Body .= $email;
+// $Body .= "\n\n";
+// $Body .= "Message: ";
+// $Body .= $message;
+
+// // send email
+// $success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// // redirect to success page
+// if ($success && $errorMSG == ""){
+//    echo "success";
+// }else{
+//     if($errorMSG == ""){
+//         echo "Something went wrong :(";
+//     } else {
+//         echo $errorMSG;
+//     }
+// }
+?>

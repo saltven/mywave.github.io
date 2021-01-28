eee1bf265d65cab1b5c8fc46d12507ca70ed0e22
salt <ammarhazrinn@gmail.com>
2021-01-28 13:39:59 +0800

    contactform

--- js/scripts.js
+++ js/scripts.js
@@ -405,48 +405,48 @@
         }
     });
 
-    // // contact form
-    // function csubmitForm() {
-    //     // initiate variables with form content
-	// 	var name = $("#cname").val();
-	// 	var email = $("#cemail").val();
-    //     var message = $("#cmessage").val();
+    // contact form
+    function csubmitForm() {
+        // initiate variables with form content
+		var name = $("#cname").val();
+		var email = $("#cemail").val();
+        var message = $("#cmessage").val();
         
-    //     $.ajax({
-    //         type: "POST",
-    //         url: "php/contactForm.php",
-    //         data: "name=" + name + "&email=" + email + "&message=" + message, 
-    //         success: function(text) {
-    //             if (text == "success") {
-    //                 cformSuccess();
-    //             } else {
-    //                 cformError();
-    //                 csubmitMSG(false, text);
-    //             }
-    //         }
-    //     });
-	// }
+        $.ajax({
+            type: "POST",
+            url: "php/contactForm.php",
+            data: "name=" + name + "&email=" + email + "&message=" + message, 
+            success: function(text) {
+                if (text == "success") {
+                    cformSuccess();
+                } else {
+                    cformError();
+                    csubmitMSG(false, text);
+                }
+            }
+        });
+	}
 
-    // function cformSuccess() {
-    //     $("#contactForm")[0].reset();
-    //     csubmitMSG(true, "Message Submitted!");
-    //     $("input").removeClass('notEmpty'); // resets the field label after submission
-    //     $("textarea").removeClass('notEmpty'); // resets the field label after submission
-    // }
+    function cformSuccess() {
+        $("#contactForm")[0].reset();
+        csubmitMSG(true, "Message Submitted!");
+        $("input").removeClass('notEmpty'); // resets the field label after submission
+        $("textarea").removeClass('notEmpty'); // resets the field label after submission
+    }
 
-    // function cformError() {
-    //     $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-    //         $(this).removeClass();
-    //     });
-	// }
+    function cformError() {
+        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).removeClass();
+        });
+	}
 
-    // function csubmitMSG(valid, msg) {
-    //     if (valid) {
-    //         var msgClasses = "h3 text-center tada animated";
-    //     } else {
-    //         var msgClasses = "h3 text-center";
-    //     }
-    //     $("#cmsgSubmit").removeClass().addClass(msgClasses).text(msg);
-    // }
+    function csubmitMSG(valid, msg) {
+        if (valid) {
+            var msgClasses = "h3 text-center tada animated";
+        } else {
+            var msgClasses = "h3 text-center";
+        }
+        $("#cmsgSubmit").removeClass().addClass(msgClasses).text(msg);
+    }
 
 })(jQuery);

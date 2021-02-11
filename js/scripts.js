9b987616b93a3996b87ab37a0206111b6874c18d
salt <ammarhazrinn@gmail.com>
2021-02-12 01:42:09 +0800

    form js

--- js/scripts.js
+++ js/scripts.js
@@ -482,52 +482,52 @@
 
     // //formspree
 
-    // window.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("DOMContentLoaded", function() {
 
     //     // get the form elements defined in your form HTML above
         
-    //     var form = document.getElementById("form-contact");
+        var form = document.getElementById("form-contact");
     //     // var button = document.getElementById("my-form-button");
-    //     var status = document.getElementById("status");
+        var status = document.getElementById("status");
     
     //     // Success and Error functions for after the form is submitted
         
-    //     function success() {
-    //         form.reset();
-    //         status.classList.add('success');
-    //         status.innerHTML = "Message sent! You will be contacted shortly.";
-    //     }
+        function success() {
+            form.reset();
+            status.classList.add('success');
+            status.innerHTML = "Message sent! You will be contacted shortly.";
+        }
     
-    //     function error() {
-    //         status.classList.add('error');
-    //         status.innerHTML = "Oops! There was a problem.";
-    //     }
+        function error() {
+            status.classList.add('error');
+            status.innerHTML = "Oops! There was a problem.";
+        }
     
     //     // handle the form submission event
     
-    //     form.addEventListener("submit", function(ev) {
-    //       ev.preventDefault();
-    //       var data = new FormData(form);
-    //       ajax(form.method, form.action, data, success, error);
-    //     });
-    //   });
-      
+        form.addEventListener("submit", function(ev) {
+            ev.preventDefault();
+            var data = new FormData(form);
+            ajax(form.method, form.action, data, success, error);
+        });
+        });
+        
     //   // helper function for sending an AJAX request
     
-    //   function ajax(method, url, data, success, error) {
-    //     var xhr = new XMLHttpRequest();
-    //     xhr.open(method, url);
-    //     xhr.setRequestHeader("Accept", "application/json");
-    //     xhr.onreadystatechange = function() {
-    //       if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    //       if (xhr.status === 200) {
-    //         success(xhr.response, xhr.responseType);
-    //       } else {
-    //         error(xhr.status, xhr.response, xhr.responseType);
-    //       }
-    //     };
-    //     xhr.send(data);
-    //   }
+        function ajax(method, url, data, success, error) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState !== XMLHttpRequest.DONE) return;
+            if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+            } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+            }
+        };
+        xhr.send(data);
+        }
     
 
 
